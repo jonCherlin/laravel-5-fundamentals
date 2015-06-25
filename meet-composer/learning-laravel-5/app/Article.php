68e03e99f75571f632ddf63baa1a7e5a015ecc16
Jon Cherlin <jonathan.cherlin@domandtom.com>
2015-06-25 15:25:13 -0400

    finished dates mutators and scopes lesson

--- meet-composer/learning-laravel-5/app/Article.php
+++ meet-composer/learning-laravel-5/app/Article.php
@@ -1,5 +1,7 @@
 <?php namespace App;
 
+use Carbon\Carbon;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model {
@@ -13,5 +15,19 @@ class Article extends Model {
 		'published_at'
 
 	];
+
+	protected $dates = ['published_at'];
+
+	public function scopePublished($query) {
+
+		$query->where('published_at', '<=', Carbon::now());
+
+	}
+
+	public function setPublishedAtAttribute($date) {
+
+		$this->attributes['published_at'] = Carbon::parse($date);
+
+ 	}
 
 }
